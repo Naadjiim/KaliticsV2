89c324a773e65e4dc49621a478c380938cfa203e
Nadjim <nadjim13-13@hotmail.fr>
2020-09-21 15:49:45 +0200

    Verion 4

--- src/Controller/EditUserController.php
+++ src/Controller/EditUserController.php
@@ -7,6 +7,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\FunctionUser;
+use App\Entity\Role;
 use App\Entity\User;
 use App\Repository\UserRepository;
 
@@ -19,6 +20,7 @@ class EditUserController extends AbstractController
     {
         $functionId = $request->request->get('function');
         $functionRepo = $manager->getRepository(FunctionUser::class);
+        $roles = $manager->getRepository(Role::class);
         $functionName = $functionRepo->findOneBy([
             'name' => $functionId
         ]);
@@ -40,8 +42,63 @@ class EditUserController extends AbstractController
 
         return $this->render('home/editUser.html.twig', [
             'functionuser' => $functionRepo->findAll(),
-            'users' => $users->findAll()
+            'users' => $users->findAll(),
+            'roles' => $roles->findAll()
+        ]);
+    }
+
+
+    /**
+     * @Route("/add/{id}", name="add_Role")
+     */
+    public function add(EntityManagerInterface $manager, Request $request, User $user){
+
+        $roleAdd = $request->request->get('function');
+        $roles = $manager->getRepository(Role::class);
+        $functionName = $user->getFunctionUser();
+        
+        if ($request->request->has('function')) 
+        {
+            $Role = [];
+            foreach($functionName->getRoles() as $role){
+                $Role[0] = $role->getName();
+                $Role[1] = $roleAdd;
+            }
+            $user->setRoles($Role);
+            $manager->persist($user);
+            $manager->flush();
+            return $this->redirectToRoute('liste');
+        }
+
+        return $this->render('home/addRole.html.twig', [
+            'roles' => $roles->findAll()
+        ]);
+    }
+
+
+     /**
+     * @Route("/remove/{id}", name="remove_Role")
+     */
+    public function remove(EntityManagerInterface $manager, Request $request, User $user){
+        $roleRemove = $request->request->get('function');
+        $functionName = $user->getFunctionUser();
+        
+        if ($request->request->has('function')) 
+        {
+            $Role = [];
+            foreach($functionName->getRoles() as $role){
+                $Role[] = $role->getName();
+                
+            }
+            unset($Role[array_search($roleRemove, $Role)]);
+            $user->setRoles($Role);
+            $manager->persist($user);
+            $manager->flush();
+        }
+
+        $roles = $manager->getRepository(Role::class);
+        return $this->render('home/removeRole.html.twig', [
+            'roles' => $roles->findAll()
         ]);
     }
 }
-
